a88f89f1fe6d38c5ed64646f91372e73b9200356
Matt Schwartz <19769905+schwartzmj@users.noreply.github.com>
2023-09-14 07:50:03 -0700

    wip

--- src/lib/components/TestScene.svelte
+++ src/lib/components/TestScene.svelte
@@ -8,6 +8,10 @@
 	let rotationZ = 0;
 	let rotationX = 90;
 
+	let cameraPositionX = -1.5;
+	let cameraPositionY = 0;
+	let cameraPositionZ = 40;
+
 	let rotationEnabled = false;
 
 	useFrame((_, delta) => {
@@ -19,9 +23,11 @@
 
 	onMount(() => {
 		document.body.prepend(controlsEle);
+		document.body.append(cameraControlsEle);
 	});
 
 	let controlsEle: HTMLDivElement;
+	let cameraControlsEle: HTMLDivElement;
 </script>
 
 <div
@@ -48,7 +54,41 @@
 	</label>
 </div>
 
-<T.PerspectiveCamera makeDefault position={[-1.5, 0, 40]} fov={10} />
+<div
+	bind:this={cameraControlsEle}
+	style="padding: 1rem; position: fixed; background: rgba(255,255,255,0.5); right:0; bottom:0 ;"
+>
+	<label>
+		Camera Position X
+		<input type="number" min="-100" max="100" step="0.1" bind:value={cameraPositionX} />
+	</label>
+	<label>
+		Camera Position Y
+		<input type="number" min="-100" max="100" step="0.1" bind:value={cameraPositionY} />
+	</label>
+	<label>
+		Camera Position Z
+		<input type="number" min="-100" max="100" step="0.1" bind:value={cameraPositionZ} />
+	</label>
+	<button
+		on:click={() => {
+			cameraPositionX = 0;
+			cameraPositionY = 0;
+			// cameraPositionZ = 0;
+			rotationX = 0;
+			rotationY = 0;
+			rotationZ = 0;
+		}}
+	>
+		Set all 0 (except camera Z)
+	</button>
+</div>
+
+<T.PerspectiveCamera
+	makeDefault
+	position={[cameraPositionX, cameraPositionY, cameraPositionZ]}
+	fov={10}
+/>
 <T.Mesh rotation.x={rotationX} rotation.y={rotationY} rotation.z={rotationZ} position.z={10}>
 	<Logo scale={1} />
 	<Environment files="/img/green-01.jpg" />
